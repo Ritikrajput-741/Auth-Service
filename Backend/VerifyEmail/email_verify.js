0ab06ae5b0999da2f769a8e7e744a8754e7fa928
Ritikrajput-741 <ritik741rajput@gmail.com>
2026-05-20 13:03:41 +0530

    fjfj

--- Backend/VerifyEmail/email_verify.js
+++ Backend/VerifyEmail/email_verify.js
@@ -4,8 +4,8 @@ export const verifyEmail = async (token, email) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp-relay.brevo.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
 
       auth: {
         user: process.env.BREVO_EMAIL,
