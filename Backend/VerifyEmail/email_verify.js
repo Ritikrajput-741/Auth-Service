4045149abce5b5261545456bb28fb24687674e7c
Ritikrajput-741 <ritik741rajput@gmail.com>
2026-05-20 12:49:56 +0530

    addd brevo

--- Backend/VerifyEmail/email_verify.js
+++ Backend/VerifyEmail/email_verify.js
@@ -1,53 +1,42 @@
 import nodemailer from "nodemailer";
 
 export const verifyEmail = async (token, email) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.example.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.USER_EMAIL,
-      pass: process.env.USER_PASS,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
 
-  console.log(process.env.USER_EMAIL);
-  console.log(process.env.USER_PASS);
-  const verificationLink = `https://auth-service-rose.vercel.app/verify/${token}`;
-  // const verificationLink = `http://localhost:5173/verify/${token}`;
+      auth: {
+        user: process.env.BREVO_EMAIL,
+        pass: process.env.BREVO_SMTP_KEY,
+      },
+    });
 
-  const mailOptions = {
-    from: process.env.USER_EMAIL,
-    to: email,
-    subject: "Verify Your Email",
+    const verificationLink = `https://auth-service-rose.vercel.app/verify/${token}`;
 
-    html: `
-      <div style="font-family: Arial; padding:20px;">
-        <h2>Email Verification</h2>
+    const mailOptions = {
+      from: process.env.BREVO_EMAIL,
+      to: email,
+      subject: "Verify Your Email",
 
-        <p>Click the button below to verify your email.</p>
+      html: `
+        <div style="font-family: Arial; padding:20px;">
+          <h2>Email Verification</h2>
 
-        <a href="${verificationLink}">
-          <button style="
-            background:black;
-            color:white;
-            padding:10px 20px;
-            border:none;
-            border-radius:5px;
-            cursor:pointer;
-          ">
+          <p>Click below to verify your email.</p>
+
+          <a href="${verificationLink}">
             Verify Email
-          </button>
-        </a>
-      </div>
-    `,
-  };
+          </a>
+        </div>
+      `,
+    };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent:", info.response);
-    }
-  });
+    const info = await transporter.sendMail(mailOptions);
+
+    console.log("Email Sent:", info.response);
+  } catch (error) {
+    console.log("Email Error:", error);
+  }
 };
