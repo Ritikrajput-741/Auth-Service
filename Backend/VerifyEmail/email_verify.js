8351a6577dc7f25fe7eb9d603ede8a926eb2b960
Ritikrajput-741 <ritik741rajput@gmail.com>
2026-05-20 11:34:09 +0530

    resend mail npm use

--- Backend/VerifyEmail/email_verify.js
+++ Backend/VerifyEmail/email_verify.js
@@ -1,53 +1,42 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const verifyEmail = async (token, email) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.USER_EMAIL,
-      pass: process.env.USER_PASS,
-    },
-  });
+  try {
+    const verificationLink =
+      `https://auth-service-rose.vercel.app/verify/${token}`;
 
-  console.log(process.env.USER_EMAIL);
-  console.log(process.env.USER_PASS);
-  const verificationLink = `https://auth-service-rose.vercel.app/verify/${token}`;
-  // const verificationLink = `http://localhost:5173/verify/${token}`;
+    const response = await resend.emails.send({
+      from: "Auth Service <onboarding@resend.dev>",
+      to: email,
+      subject: "Verify Your Email",
 
-  const mailOptions = {
-    from: process.env.USER_EMAIL,
-    to: email,
-    subject: "Verify Your Email",
+      html: `
+        <div style="font-family: Arial; padding:20px;">
+          <h2>Email Verification</h2>
 
-    html: `
-      <div style="font-family: Arial; padding:20px;">
-        <h2>Email Verification</h2>
+          <p>Click the button below to verify your email.</p>
 
-        <p>Click the button below to verify your email.</p>
+          <a href="${verificationLink}">
+            <button style="
+              background:black;
+              color:white;
+              padding:10px 20px;
+              border:none;
+              border-radius:5px;
+              cursor:pointer;
+            ">
+              Verify Email
+            </button>
+          </a>
+        </div>
+      `,
+    });
 
-        <a href="${verificationLink}">
-          <button style="
-            background:black;
-            color:white;
-            padding:10px 20px;
-            border:none;
-            border-radius:5px;
-            cursor:pointer;
-          ">
-            Verify Email
-          </button>
-        </a>
-      </div>
-    `,
-  };
+    console.log(response);
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent:", info.response);
-    }
-  });
+  } catch (error) {
+    console.log("Email Error:", error);
+  }
 };
